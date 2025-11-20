23d7c1d4f38893a16d6103e5c1e9e81d1f16363d
Yanan2236 <kent31415926535@gmail.com>
2025-11-20 14:27:22 +0000

    feat(admin): implement admin configuration for all models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Restaurant, OpeningHour, Category, Review, Reservation, Favorite, Subscription
 
-# Register your models here.
+
+class OpeningHourInine(admin.TabularInline):
+    model = OpeningHour
+    extra = 1
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    inlines = [OpeningHourInine]
+    list_filter = ("category",)
+    filter_horizontal = ("category",)
+
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Category)
+admin.site.register(Review)
+admin.site.register(Reservation)
+admin.site.register(Favorite)
+admin.site.register(Subscription)
